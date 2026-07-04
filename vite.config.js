f3e43872de009a0ea78bd23c553567b1652c169f
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2026-07-04 22:16:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures assets are resolved relative to the deployment index.html
+  build: {
+    outDir: 'dist', // The output directory where production files are built
+  }
 })
